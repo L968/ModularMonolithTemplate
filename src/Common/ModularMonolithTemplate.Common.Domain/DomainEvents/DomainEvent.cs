f8c60a579a3a98f40ec332df93fffd7cee3e158b
Lucas Constantino <lucas.c968@gmail.com>
2025-03-09 23:29:01 -0300

    Bug fix in outbox message deserialization

--- src/Common/ModularMonolithTemplate.Common.Domain/DomainEvents/DomainEvent.cs
+++ src/Common/ModularMonolithTemplate.Common.Domain/DomainEvents/DomainEvent.cs
@@ -2,6 +2,6 @@
 
 public abstract record DomainEvent : IDomainEvent
 {
-    public Guid Id { get; } = Guid.NewGuid();
-    public DateTime OccurredOnUtc { get; } = DateTime.UtcNow;
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public DateTime OccurredOnUtc { get; init; } = DateTime.UtcNow;
 }
